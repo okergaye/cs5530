bb2444a900d98b1c520054faa20cfd9fde746422
Onoko123 <Onoko123@gmail.com>
2018-03-06 12:32:23 -0700

    started on UberUserCode

---
+++ CS5530/src/cs5530FinalProj/UberCar.java
@@ -0,0 +1,43 @@
+package cs5530FinalProj;
+
+import java.sql.*;
+
+public class UberCar 
+{
+	public UberCar()
+	{}
+	
+	public String getUberDriver(String login, String UUID, Statement stmt)
+	{
+		String sql="";
+		String output="";
+		ResultSet rs=null;
+	 	System.out.println("executing "+sql);
+	 	try
+	 	{
+	 		rs=stmt.executeQuery(sql);
+	 		while (rs.next())
+	 		{
+	 		}
+	     
+	 		rs.close();
+	 	}
+	 	catch(Exception e)
+	 	{
+	 		System.out.println("cannot execute the query");
+	 	}
+	 	finally
+	 	{
+	 		try
+	 		{
+		 		if (rs!=null && !rs.isClosed())
+		 			rs.close();
+	 		}
+	 		catch(Exception e)
+	 		{
+	 			System.out.println("cannot close resultset");
+	 		}
+	 	}
+	    return output;
+	}
+}
---
+++ CS5530/src/cs5530FinalProj/UberUser.java
@@ -0,0 +1,71 @@
+package cs5530FinalProj;
+
+import java.sql.*;
+
+public class UberUser 
+{
+	public UberUser()
+	{}
+	
+	public String getUberUser(String login, String password, Statement stmt)
+	{
+		String sql="select uuid from UberUser where login = '%"+login+"%' and password = '%"+password+"%'";
+		String output="";
+		ResultSet rs=null;
+	 	System.out.println("executing "+sql);
+	 	try
+	 	{
+	 		rs=stmt.executeQuery(sql);
+	 		while (rs.next())
+	 		{
+	 			output += rs.getString("uuid")+"\n";
+	 		}
+	     
+	 		rs.close();
+	 	}
+	 	catch(Exception e)
+	 	{
+	 		System.out.println("cannot execute the query");
+	 	}
+	 	finally
+	 	{
+	 		try
+	 		{
+		 		if (rs!=null && !rs.isClosed())
+		 			rs.close();
+	 		}
+	 		catch(Exception e)
+	 		{
+	 			System.out.println("cannot close resultset");
+	 		}
+	 	}
+	    return output;
+	}
+	
+	public int createUser(String login, String password, String name, String address, String phone, Statement stmt)
+	{
+		String sql = "insert into Users values ('%" + login + "%', '%" + name + "%', '%" + password + "%',"
+				+ " '%" + address + "%', '%" + phone + "%')";
+		int output = -1;
+		try
+		{
+			output = stmt.executeUpdate(sql);
+		}
+		catch(Exception e)
+		{
+			System.out.println("cannot execute the query");
+			System.out.println(e.getMessage());
+		}
+
+		if (output > 0)
+		{
+			System.out.println("User Creation Successful");
+			return 1;
+		}
+		else
+		{
+			System.out.println("User Creation Failed");
+			return 0;
+		} 	
+	}
+}
